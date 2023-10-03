18d0425fecbf8d455d1be0d0d70083835b65ea71
Rafael Douglas <rafael.douglas.1@gmail.com>
2023-10-03 10:52:49 -0300

    Inserir última atualização na URL

--- refresher.js
+++ refresher.js
@@ -1,8 +1,18 @@
 const delay = 300000;
+urlOg = window.location.href;
+var dataAtual = new Date();
+var horaLocal = dataAtual.getHours();
+var minutosLocal = dataAtual.getMinutes();
+var segundosLocal = dataAtual.getSeconds();
+window.location.replace(splitUrl(urlOg)+'#'+horaLocal+':'+minutosLocal);
 
 function refreshPage(){
-    console.log('entrou na function');
     	location.reload();
+};
+
+function splitUrl(url){
+    var partes = url.split('#');
+    return partes[0]
 };
 
 setInterval(refreshPage, delay);
